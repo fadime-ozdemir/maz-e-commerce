dfb90e60e6b903cd7dfa88fcd1f75e11d265063a
fadime94 <fadime.ozdemir11@gmail.com>
2020-12-12 22:00:33 +0300

    add footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 export default function Footer() {
     return (
-        <div>
-            
-        </div>
+        <Container fluid>
+            <Navbar className="d-flex justify-content-around">
+            <NavLink to="/" className="navbar-brand">
+                <Navbar.Brand>Bibi's</Navbar.Brand>
+            </NavLink>
+                <Nav className="ml-auto">
+                    <NavLink to="/shirts" className="nav-link">
+                        Shirts
+                    </NavLink>
+                    <NavLink to="/cart" className="nav-link">
+                        Cart
+                    </NavLink>
+                </Nav>
+        </Navbar>
+        </Container>
     )
 }
