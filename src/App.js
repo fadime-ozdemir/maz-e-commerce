f45df14b1b87472e0e6caa79cb96ba4e4e47def1
fadime94 <fadime.ozdemir11@gmail.com>
2020-12-12 14:39:17 +0300

    add main routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from "./components/header";
+import MainNavbar from "./components/mainNavbar";
+import Footer from "./components/footer";
+import ShirtPage from "./containers/shirtPage";
+import ShirtsCategory from "./containers/shirtsCategory";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <MainNavbar />
+      <Switch className="App">
+          <Route path="/shirts">
+            <ShirtsCategory />
+          </Route>
+          <Route path="/shirtA">
+            <ShirtPage />
+          </Route>
+          <Route path="/">
+            <Header />
+          </Route>
+        </Switch>
+      <Footer />
+    </Router>
   );
 }
 
